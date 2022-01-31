263ccdeaf63b118dac9cec902090c4085a5bf1ab
Nijin Nazar <63832147+nijin26@users.noreply.github.com>
2022-01-31 23:45:27 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeToRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+function changeToViolet() {
+  document.body.style.backgroundColor = "violet";
+}
+
+function changeToBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function changeToBlack() {
+  document.body.style.backgroundColor = "black";
+}
